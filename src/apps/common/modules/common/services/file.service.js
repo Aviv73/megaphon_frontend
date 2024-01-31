d52fa5bf9db9d45f3bc764e2b7624e0f0f5b7e2e
Aviv Issachar <avivi@misterbit.co.il>
2024-01-31 16:33:37 +0200

    ..

--- src/apps/common/modules/common/services/file.service.js
+++ src/apps/common/modules/common/services/file.service.js
@@ -12,8 +12,8 @@ export function uploadFileToServer(formData) {
 }
 
 export function fixFileSrcToThumbnail(url = '') {
-  return url;
-  // return url.replace('megaphonecs.s3.amazonaws.com/uploads', 'images.megaphon.co.il');
+  // return url;
+  return url.replace('megaphonecs.s3.amazonaws.com/uploads', 'images.megaphon.co.il');
 }
 
 export function loadStaticFile(filePath) {
