960ee006099627ee8511f32c45bde5e1cf0099db
Aviv Issachar <avivi@misterbit.co.il>
2024-06-04 16:23:23 +0300

    ..

--- src/apps/megaphonApp/modules/common/services/template.util.service.js
+++ src/apps/megaphonApp/modules/common/services/template.util.service.js
@@ -145,7 +145,7 @@ function _getDefaultTEmplatesData() {
               "DEFAULT_GROUP_RELEASE"
           ],
           // "handlebarsLocalFilePath" : "default/templates/group/default.group.email"
-          "handlebarsLocalFilePath" : "default/templates/release.simple.email"
+          "handlebarsLocalFilePath" : "default/templates/default.release.email"
       },
       {
           "name" : "דיפולטיבי - ניוזלטר רליס פשוט",
@@ -156,7 +156,7 @@ function _getDefaultTEmplatesData() {
               "DEFAULT_SIMPLE_RELEASE"
           ],
           // "handlebarsLocalFilePath" : "default/templates/simple/default.simple.email"
-          "handlebarsLocalFilePath" : "default/templates/release.simple.email"
+          "handlebarsLocalFilePath" : "default/templates/default.release.email"
       }
     ],
   
