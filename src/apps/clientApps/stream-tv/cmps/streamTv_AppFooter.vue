b3d60d1be8328034a227719c3724728dcc938466
Aviv Issachar <avivi@misterbit.co.il>
2024-08-19 21:46:00 +0300

    ..

--- src/apps/clientApps/stream-tv/cmps/streamTv_AppFooter.vue
+++ src/apps/clientApps/stream-tv/cmps/streamTv_AppFooter.vue
@@ -1,7 +1,9 @@
 <template>
   <footer class="app-footer">
     <div class="container flex space-between">
-      <div class="ph"></div>
+      <div>
+        {{org?.designPreferences?.contactMsg || ''}}
+      </div>
       <div class="flex align-center gap20">
         <span>{{$t('contact')}} {{org.name}}</span>
         <OrgMediaLinks :organization="org" :color="selectedTheme?.colors?.[2]"/>
@@ -20,7 +22,6 @@ export default {
       return this.$store.getters['organization/selectedItem'] || {};
     },
     selectedTheme() {
-      console.log(this.$store.getters['selectedTheme']);
       return this.$store.getters['selectedTheme'];
     }
   }
