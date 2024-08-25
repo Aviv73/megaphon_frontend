ff9d559b70d153afe6b53c3c8a40b8c713b6e26c
Aviv Issachar <avivi@misterbit.co.il>
2024-08-25 15:19:06 +0300

    ..

--- src/apps/megaphonApp/modules/release/views/ReleaseEdit.vue
+++ src/apps/megaphonApp/modules/release/views/ReleaseEdit.vue
@@ -157,7 +157,7 @@ export default {
 
 
     onFileUploaded(file) {
-      delete file.src;
+      // delete file.src;
       if (this.itemToEdit._id) return;
       if (!this.itemToEdit.__newFiles__) this.itemToEdit.__newFiles__ = [];
       this.itemToEdit.__newFiles__.push(file);
