2fa0398f68b792a42a031461d89b007d10663325
Aviv Issachar <avivi@misterbit.co.il>
2024-10-26 22:21:51 +0300

    ..

--- src/apps/common/modules/common/services/file.service.js
+++ src/apps/common/modules/common/services/file.service.js
@@ -41,55 +41,68 @@ export async function chunkUploadFileToServer(file, organizationId, parentData, 
   const fileSize = file.size;
   const originalName = file.name;
 
+  const totalChunks = Math.ceil(fileSize / CHUNK_SIZE);  
+
   const lastDotIdx = file.name.lastIndexOf('.');
   const type = lastDotIdx > -1 ? file.name.substring(lastDotIdx+1) : file.mimetype?.split('/').pop();
   const storeFileName = `file-${getRandomId()}.${type}`;
   
-  // const prms = [];
+  const prms = [];
+  const baseParams = {parentData, fileSize, totalChunks, storeFileName, originalName, chunkSize: CHUNK_SIZE};
   let uploadId;
+  const chunkByChunkMode = false;
+  if (!chunkByChunkMode) uploadId = await httpService.post(`${ENDPOINT}/openMultipartUpload/${organizationId}`, null, baseParams);
   let res;
   let uploadedBytes = 0;
   let socketRoom = '';
   // const isEncryptionMode = false;
   let doneChunksCount = 0;
-  const totalChunks = Math.ceil(fileSize / CHUNK_SIZE);  
   for (let i = 0; i < totalChunks; i++) {
-    const start = i * CHUNK_SIZE;
-    const end = Math.min(start + CHUNK_SIZE, fileSize);
-    const chunk = file.slice(start, end);
-    const formData = new FormData();
-    formData.append('file', chunk);
-    const params = {parentData, fileSize, totalChunks, uploadId, chunkIdx: i, storeFileName, originalName, chunkSize: CHUNK_SIZE, range: {start, end}};
-    if (!i) params.isFirst = true;
-    if (i === totalChunks - 1) params.isLast = true;
-    const prm = httpService.post(`${ENDPOINT}/upload/${organizationId}`, formData, params);
-    const currRes = await prm;
-    if (!i) { // if firstTime, if no uploadId
-      // res = currRes;
-      uploadId = currRes.uploadId;
-    } 
-
-    uploadedBytes += CHUNK_SIZE;
-    doneChunksCount++;
-    // onChunkEndCb?.({ uploadedBytes, totalSize: fileSize, percents: Math.min((uploadedBytes / fileSize)*100, 100), msg: '' });
-    onChunkEndCb?.({ uploadedBytes, totalSize: fileSize, percents: Math.min((doneChunksCount / totalChunks)*100, 100), msg: '' });
-
-    if (i === totalChunks - 1) {
-      res = currRes;
-      // if (isEncryptionMode) {
-      //   socketRoom = currRes.socketRoom;
-      //   // socketService.emit('join-room', socketRoom);
-      //   onChunkEndCb?.({ uploadedBytes, totalSize: fileSize, percents: Math.min((uploadedBytes / fileSize)*100, 100), msg: 'encrypting' });
-      //   res = await (new Promise((resolve, reject) => {
-      //     // socketService.on('encryption-finished', data => resolve(data));
-      //   }));
-      // }
-    } else {
+    const doIt = async () => {
+      const start = i * CHUNK_SIZE;
+      const end = Math.min(start + CHUNK_SIZE, fileSize);
+      const chunk = file.slice(start, end);
+      const formData = new FormData();
+      formData.append('file', chunk);
+      const params = {...baseParams, uploadId, chunkIdx: i, range: {start, end}, chunkByChunkMode};
+      if (!i) params.isFirst = true;
+      if (i === totalChunks - 1) params.isLast = true;
+      const currRes = await httpService.post(`${ENDPOINT}/upload/${organizationId}`, formData, params);
+      if (!uploadId) { // if firstTime, if no uploadId
+        // res = currRes;
+        uploadId = currRes.uploadId;
+      } 
+  
+      uploadedBytes += CHUNK_SIZE;
+      doneChunksCount++;
+      // onChunkEndCb?.({ uploadedBytes, totalSize: fileSize, percents: Math.min((uploadedBytes / fileSize)*100, 100), msg: '' });
+      onChunkEndCb?.({ uploadedBytes, totalSize: fileSize, percents: Math.min((doneChunksCount / totalChunks)*100, 100), msg: '' });
+  
+      if (i === totalChunks - 1) {
+        if (chunkByChunkMode) {
+          res = currRes;
+        }
+        // if (isEncryptionMode) {
+        //   socketRoom = currRes.socketRoom;
+        //   // socketService.emit('join-room', socketRoom);
+        //   onChunkEndCb?.({ uploadedBytes, totalSize: fileSize, percents: Math.min((uploadedBytes / fileSize)*100, 100), msg: 'encrypting' });
+        //   res = await (new Promise((resolve, reject) => {
+        //     // socketService.on('encryption-finished', data => resolve(data));
+        //   }));
+        // }
+      } else {
+      }
     }
-    // prms.push(prm);
-    uploadedBytes += CHUNK_SIZE;
+    const prm = doIt();
+    if (chunkByChunkMode) await prm;
+    else prms.push(prm);
+    // uploadedBytes += CHUNK_SIZE;
   }
-  // await Promise.all(prms);
+  if (!chunkByChunkMode) {
+    await Promise.all(prms);
+    console.log('WOW?');
+    res = await httpService.post(`${ENDPOINT}/closeMultipartUpload/${organizationId}`, null, {...baseParams, uploadId});
+  }
   return res;
 }
 
