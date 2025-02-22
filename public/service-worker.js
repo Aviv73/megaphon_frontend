3053947d7c610233620003892770dec1eacda054
Aviv Issachar <avivi@misterbit.co.il>
2025-02-22 16:01:29 +0200

    updated service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,6 +14,21 @@ workbox.routing.setDefaultHandler(
   new workbox.strategies.NetworkOnly()
 );
 
+// Clear all caches on activation
+self.addEventListener('activate', async (event) => {
+  event.waitUntil(
+      (async () => {
+          const cacheNames = await caches.keys();
+          await Promise.all(cacheNames.map(cache => caches.delete(cache)));
+          console.log('All caches cleared');
+
+          // // Force all clients to reload
+          // const clients = await self.clients.matchAll({ type: 'window', includeUncontrolled: true });
+          // clients.forEach(client => client.navigate(client.url));
+      })()
+  );
+});
+
 // If you have other routes with specific strategies, you can add them here
 // For example, you can keep the cache-first strategy for images if desired
 // workbox.routing.registerRoute(
